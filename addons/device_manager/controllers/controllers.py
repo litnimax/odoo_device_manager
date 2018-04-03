35319418e87a028e7c0c022c0c19d3b31a55ea69
Maxim Litnitskiy <litnimaxster@gmail.com>
2018-04-03 18:47:53 +0300

    register refactor

--- addons/device_manager/controllers/controllers.py
+++ addons/device_manager/controllers/controllers.py
@@ -12,27 +12,25 @@ logger = logging.getLogger(__name__)
 class Container(http.Controller):
      @http.route('/device_manager/register', auth='public', type='json')
      def register(self):
-        token = http.request.jsonrequest.get('token')
         uid = http.request.jsonrequest.get('uid')
-        app = http.request.env['device_manager.application'].sudo().search(
-                                            [('token','=', token)])
-        if not app:
-            raise Warning('Application token not found')
-        # Now check if the device is already registered
+        # Check if the device is already registered
         device = http.request.env['device_manager.device'].sudo().search(
             [('uid','=',uid)])
         if device:
-            # Check application
-            if device.application.id != app.id:
-                logger.warning('Device {} already registered with app {}'.format(
-                    uid, device.application.name))
-                raise Warning('Device {} already registered to app {}'.format(
-                    uid, device.application.name))
-            else:
-                device.last_online = fields.Datetime.now()
-                logger.debug(
-                    'Device {} is already registered'.format(uid))
+            logger.debug('Device already registered')
+            device.last_online = fields.Datetime.now()
+            pw = http.request.jsonrequest.get('password')
+            if pw:
+                #Reset device password
+                device.set_password = pw
+                logger.info('Device {} password reset'.format(uid))
         else:
+            # Device not found, try token registration
+            token = http.request.jsonrequest.get('token')        
+            app = http.request.env['device_manager.application'].sudo().search(
+                                                [('token','=', token)])
+            if not app:
+                raise Warning('Application token not found')
             device = http.request.env['device_manager.device'].register(app,
                                                     http.request.jsonrequest)
         return {
