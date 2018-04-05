418745576a3f631f39da39f52f263a947d56287c
Serg Truf <strufkin@gmail.com>
2018-04-05 20:50:03 +0300

    Added broker settings/certificate support for MQTT connections

--- addons/device_manager/controllers/controllers.py
+++ addons/device_manager/controllers/controllers.py
@@ -13,13 +13,13 @@ class Container(http.Controller):
      @http.route('/device_manager/register', auth='public', type='json')
      def register(self):
         uid = http.request.jsonrequest.get('uid')
+        pw = http.request.jsonrequest.get('password')
         # Check if the device is already registered
         device = http.request.env['device_manager.device'].sudo().search(
             [('uid','=',uid)])
         if device:
             logger.debug('Device already registered')
             device.last_online = fields.Datetime.now()
-            pw = http.request.jsonrequest.get('password')
             if pw:
                 #Reset device password
                 device.set_password = pw
@@ -33,17 +33,29 @@ class Container(http.Controller):
                 raise Warning('Application token not found')
             device = http.request.env['device_manager.device'].register(app,
                                                     http.request.jsonrequest)
+                                                    
+        mqtt_host = http.request.env[
+                        'device_manager.settings']._get_param('mqtt_host')
+        mqtt_port = http.request.env[
+                        'device_manager.settings']._get_param('mqtt_port')
+        cafile = http.request.env[
+                        'device_manager.settings']._get_param('cafile')
+        capath = http.request.env[
+                        'device_manager.settings']._get_param('capath')
+        cadata = http.request.env[
+                        'device_manager.settings']._get_param('cadata')
+        mqtt_scheme = 'mqtts' if cafile or capath or cadata else 'mqtt'
+        if int(mqtt_port):
+            mqtt_host = '{}:{}'.format(mqtt_host, mqtt_port)
+
         return {
-            'mqtt_host': http.request.env[
-                        'device_manager.settings']._get_param('mqtt_host'),
-            'mqtt_port': http.request.env[
-                        'device_manager.settings']._get_param('mqtt_port'),
-            'cafile': http.request.env[
-                        'device_manager.settings']._get_param('cafile'),
-            'capath': http.request.env[
-                        'device_manager.settings']._get_param('capath'),
-            'cadata': http.request.env[
-                        'device_manager.settings']._get_param('cadata'),
+            'broker' : {
+                'uri' : '{}://{}:{}@{}'.format(mqtt_scheme, device.username,
+                                                pw, mqtt_host),
+                'cafile' : cafile,
+                'cadata' : cadata,
+                'capath' : capath
+            },
             'device_id': device.id,
         }
 
