30650c5ce15464e905bbdc4163e0dc288d81d70d
Maxim Litnitskiy <litnimaxster@gmail.com>
2018-04-19 09:50:03 +0300

    Forgotten files

--- mosquitto/addons/mosquitto/__manifest__.py
+++ mosquitto/addons/mosquitto/__manifest__.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 {
-    'name': 'Mosquitto',
-    'description': 'Mosquitto Odoo UI',
-    'version': '1.0',
-    'author': 'Max',
-    'website': 'http://comlib.md',
+    'name': 'Eclipse Mosquitto™ Manager',
+    'summary': 'An open source MQTT broker management interface',
+    'description': '''An open source MQTT broker management interface for 
+Eclipse Mosquitto, an open source (EPL/EDL licensed) message broker that implements the MQTT protocol versions 3.1 and 3.1.1. Mosquitto is lightweight and is suitable for use on all devices from low power single board computers to full servers.
+The MQTT protocol provides a lightweight method of carrying out messaging using a publish/subscribe model. This makes it suitable for Internet of Things messaging such as with low power sensors or mobile devices such as phones, embedded computers or microcontrollers.
+''',
+    'version': '1.0',    
+    'author': 'Odooino LLC',
+    'website': 'http://odooino.com',
     'license': 'AGPL-3',
     'category': 'Web',
     'depends': [
@@ -22,4 +26,6 @@
         'views/acl.xml',
     ],
     'installable': True,
+    'price': 101,
+    'currency': 'EUR',
 }
